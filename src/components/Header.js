4d1abd9b965a642dcec539b2d5340baaf417621a
John Angle <jmangle93@gmail.com>
2022-05-22 18:24:00 -0600

    header color, very important

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ function Header({clearBuzzins}) {
   return (
     <div>
       <div>
-        <h1 className='mt-4' align='center'>Jepperdee!</h1>
+        <h1 className='mt-4' align='center' ><font color="#23c">Jepperdee!</font></h1>
       </div>
       <div className='row justify-content-between align-items-center mt-4'>
         <button type="button" onClick={() => listenBuzzins()}>Listen For Buzzins</button>
